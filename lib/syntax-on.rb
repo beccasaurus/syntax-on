6f6931d1ff1f56cda02eb8173b7629b46b6d6c5c
remi <remi@remitaylor.com>
2008-05-08 23:07:39 -0700

    got the very basics working, including modeline reading and explicit syntax ... next up: themes

---
+++ lib/syntax-on.rb
@@ -0,0 +1,67 @@
+$:.unshift File.dirname(__FILE__)
+
+require 'fileutils'
+
+class Array
+  def to_vim_args
+    self.map{ |option| %{+"#{ option.gsub('"',"'") }"} }.join ' '
+  end
+end
+
+class SyntaxOn
+
+  VIM_BIN         = 'vim'
+  VIM_OPTIONS     = [ "syntax on", "set nonumber", "exe 'normal zR'" ]
+  VIM_RENDER      = [ "runtime\\! syntax/2html.vim", "wq", "q" ]
+  TEMP_DIRECTORY  = '/tmp/syntax-on'
+  TEMP_FILENAME   = lambda { Time.now.strftime '%Y-%d-%m_%Hh-%Mm-%Ss' }
+
+  attr_accessor :code, :syntax
+
+  def initialize code, options = { :syntax => nil }
+    @code   = code
+    @syntax = options[:syntax]
+  end
+
+  def to_html options = { :theme => nil, :line_numbers => true }
+    setup_temp_dir
+    create_temp_file
+    setup_vim_options
+    render
+    @html = File.read(@html_file).match(/<pre>(.*)<\/pre>/m)[1].strip
+  end
+
+  private
+
+  def setup_temp_dir
+    FileUtils.mkdir_p TEMP_DIRECTORY
+    FileUtils.cd      TEMP_DIRECTORY
+  end
+
+  def create_temp_file
+    @filename = File.join TEMP_DIRECTORY, TEMP_FILENAME.call
+    File.open(@filename, 'w'){|f| f << @code }
+  end
+
+  def setup_vim_options
+    @options = VIM_OPTIONS.clone
+    @options << "setfiletype #{ @syntax.to_s }" if @syntax
+  end
+
+  def command_string
+    "#{ VIM_BIN } #{ @options.to_vim_args } #{ VIM_RENDER.to_vim_args } #{ @filename } &>/dev/null"
+  end
+
+  def render
+    @output = `#{ command_string }`
+    @html_file = @filename + '.html'
+  end
+
+end
+
+rc = File.expand_path '~/.syntaxonrc'
+eval File.read(rc) if File.file? rc
+
+unless `which #{ SyntaxOn::VIM_BIN }`.strip =~ /^\/(.*)#{ SyntaxOn::VIM_BIN }$/
+  puts "SyntaxOn WARNING: VIM_BIN[#{ SyntaxOn::VIM_BIN.inspect }] not found in PATH"
+end
